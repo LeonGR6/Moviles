53cb9d6c6bcc2a6f05a18a882b1bb6dfed9e04fc
marrrx <antonio532@live.com>
2024-04-09 11:11:08 -0600

    validaciones

--- views/login.tsx
+++ views/login.tsx
@@ -4,125 +4,107 @@ import Auth_layout from './layouts/auth';
 import { useState } from 'react';
 import { ErrorContext } from '../context/errorContext';
 import { useAuthentication } from '../auth/hooks/useAuthentication';
+import { Formik } from 'formik';
 
 const Login = ({ navigation }: { navigation: any }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
-    const { errors, clearErrors } = useContext(ErrorContext);
-
-
-    const handleState = () => {
-        setShowPassword((showState) => {
-            return !showState
-        })
-    }
-
+    const { clearErrors } = useContext(ErrorContext);
     const { signIn } = useAuthentication();
 
-    const onHandleSignIn = () => {
-        clearErrors();
-        signIn({ email, password });
+    const handleState = () => {
+        setShowPassword((showState) => !showState);
     }
+
     const handleNavigate = () => {
         clearErrors();
         navigation.navigate('Register');
     };
 
     return (
-
         <>
             <Auth_layout />
-
-            <Box
-                mt={5}
-            >
-                <FormControl
-                    p="$4"
-                    borderRadius="$lg"
+            <Box mt={5}>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validate={values => {
+                        const errors: any = {};
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                            errors.email = 'Invalid email address';
+                        }
+                        if (!values.password) {
+                            errors.password = 'Required';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={async (values, { setSubmitting }) => {
+                        clearErrors();
+                        await signIn(values);
+                        setSubmitting(false);
+                    }}
                 >
-                    <VStack
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                        space="lg"
-                        mt={15}
-                    >
-                        <Heading $dark-color="$textLight200" size='2xl'  style={{ textAlign: 'center' }}>
-                            SubstanceTwins
-                        </Heading>
-                        <Text $dark-color="$textLight200" size='xl' style={{ textAlign: 'center' }}>
-                            Your body must be prepared for any situation, which is why you need supplements.
-                        </Text>
-                        {errors ? errors.map(error => <Alert h={10} bgColor="$red300" variant="solid" key={error}><AlertText>{error}</AlertText></Alert>) : null}
-
-                        <Input w={270} variant="rounded"  >
-                            <InputField
-                                placeholder="Email"
-                                type="text"
-                                value={email}
-                                onChangeText={setEmail}
-                            />
-                        </Input>
-                    </VStack>
-
-                    <VStack
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                        space="lg"
-                        mt={22}
-                    >
-                        <Input
-                            w={270}
-                            variant="rounded"
-                        >
-                            <InputField
-                                placeholder="Password"
-                                type={showPassword ? "text" : "password"}
-                                value={password}
-                                onChangeText={setPassword}
-                            />
-                            <InputSlot
-                                pr="$3"
-                                onPress={handleState}
-                            >
-                                <InputIcon
-                                    as={showPassword ? EyeIcon : EyeOffIcon}
-                                    color="$yellow500"
-                                />
-                            </InputSlot>
-                        </Input>
-                    </VStack>
-
-                    <VStack
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                        space="lg"
-                        mt={22}
-                        mb="10%"
-                        >
-                        <Button
-                            bg="$yellow500"
-                            $active-bg="$yellow600"
-                            w={90}
-                            size="sm"
-                            variant="solid"
-                            isDisabled={false}
-                            onPress={onHandleSignIn}
-                        >
-                            <ButtonText>Login</ButtonText>
-                        </Button>
-                        <Link onPress={handleNavigate}>
-                            <LinkText>Don´t have a account? click here to register</LinkText>
-                        </Link>
-                    </VStack>
-
-
-                </FormControl>
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                        <FormControl p="$4" borderRadius="$lg">
+                            <VStack alignItems='center' justifyContent='center' space="lg" mt={15}>
+                                <Heading $dark-color="$textLight200" size='2xl' style={{ textAlign: 'center' }}>
+                                    SubstanceTwins
+                                </Heading>
+                                <Text $dark-color="$textLight200" size='xl' style={{ textAlign: 'center' }}>
+                                    Your body must be prepared for any situation, which is why you need supplements.
+                                </Text>
+                                {errors.email && touched.email && <Alert h={10} bgColor="$red300" variant="solid"><AlertText>{errors.email}</AlertText></Alert>}
+                                <Input w={270} variant="rounded">
+                                    <InputField
+                                        placeholder="Email"
+                                        type="text"
+                                        onChangeText={handleChange('email')}
+                                        onBlur={handleBlur('email')}
+                                        value={values.email}
+                                    />
+                                </Input>
+                                {errors.password && touched.password && <Alert h={10} bgColor="$red300" variant="solid"><AlertText>{errors.password}</AlertText></Alert>}
+                                <Input w={270} variant="rounded">
+                                    <InputField
+                                        placeholder="Password"
+                                        type={showPassword ? "text" : "password"}
+                                        onChangeText={handleChange('password')}
+                                        onBlur={handleBlur('password')}
+                                        value={values.password}
+                                    />
+                                    <InputSlot pr="$3" onPress={handleState}>
+                                        <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} color="$yellow500" />
+                                    </InputSlot>
+                                </Input>
+                                <Button
+                                    bg="$yellow500"
+                                    $active-bg="$yellow600"
+                                    w={90}
+                                    size="sm"
+                                    variant="solid"
+                                    onPress={() => handleSubmit()}
+                                    disabled={isSubmitting}
+                                >
+                                    <ButtonText>{isSubmitting ? 'Logging in...' : 'Login'}</ButtonText>
+                                </Button>
+                                <Link onPress={handleNavigate}>
+                                    <LinkText>Don't have an account? Click here to register</LinkText>
+                                </Link>
+                            </VStack>
+                        </FormControl>
+                    )}
+                </Formik>
             </Box>
         </>
-    )
+    );
 };
+
 export default Login;
--- views/register.tsx
+++ views/register.tsx
@@ -1,30 +1,18 @@
-import { Box, Input, InputField, VStack, Text, InputIcon, InputSlot, FormControl, Heading, EyeIcon, EyeOffIcon, Button, ButtonText, ButtonIcon, AddIcon, Link, LinkText, Alert, AlertText } from '@gluestack-ui/themed';
+import React, { useContext } from 'react';
+import { Box, Input, InputField, VStack, InputIcon, InputSlot, FormControl, EyeIcon, Text, Heading, EyeOffIcon, Button, ButtonText, Link, LinkText, Alert, AlertText } from '@gluestack-ui/themed';
 import Auth_layout from './layouts/auth';
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 import { ErrorContext } from '../context/errorContext';
 import { useAuthentication } from '../auth/hooks/useAuthentication';
-import React from 'react';
-
+import { Formik } from 'formik';
 
 const Register = ({ navigation }: { navigation: any }) => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [password_confirmation, setConfirmPassword] = useState('');
-    const { errors, clearErrors } = useContext(ErrorContext);
-
-
-    const [showPassword, setShowPassword] = useState(false)
-    const handleState = () => {
-        setShowPassword((showState) => {
-            return !showState
-        })
-    }
+    const [showPassword, setShowPassword] = useState(false);
+    const { clearErrors } = useContext(ErrorContext);
     const { signUp } = useAuthentication();
 
-    const onHandleSignUp = () => {
-        clearErrors();
-        signUp({ name, email, password, password_confirmation });
+    const handleState = () => {
+        setShowPassword((showState) => !showState);
     }
 
     const handleNavigate = () => {
@@ -34,144 +22,120 @@ const Register = ({ navigation }: { navigation: any }) => {
 
     return (
         <>
-
             <Auth_layout />
-
-            <Box
-                mt={5}
-            >
-                <FormControl
-                    p="$4"
+            <Box mt={5}>
+                <Formik
+                    initialValues={{ name: '', email: '', password: '', password_confirmation: '' }}
+                    validate={values => {
+                        const errors: any = {};
+                        if (!values.name) {
+                            errors.name = 'Required';
+                        }
+                        if (!values.email) {
+                            errors.email = 'Required';
+                        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                            errors.email = 'Invalid email address';
+                        }
+                        if (!values.password) {
+                            errors.password = 'Required';
+                        }
+                        if (!values.password_confirmation) {
+                            errors.password_confirmation = 'Required';
+                        } else if (values.password_confirmation !== values.password) {
+                            errors.password_confirmation = 'Passwords do not match';
+                        }
+                        return errors;
+                    }}
+                    onSubmit={async (values, { setSubmitting }) => {
+                        clearErrors();
+                        await signUp(values);
+                        setSubmitting(false);
+                    }}
                 >
-                    <VStack
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                        space="lg"
-                        mt={15}
-                    >
-                        <Heading $dark-color="$textLight200" size='2xl' style={{ textAlign: 'center' }}>
-                            SubstanceTwins
-                        </Heading>
-                        <Text $dark-color="$textLight200" size='xl' style={{ textAlign: 'center' }}>
-                            Your body must be prepared for any situation, which is why you need supplements.
-                        </Text>
-                        {errors ? errors.map(error => <Alert h={10} bgColor="$red300" variant="solid" key={error}><AlertText>{error}</AlertText></Alert>) : null}
-
-
-                        <Input w={270} variant="rounded" >
-                            <InputField
-                                placeholder="Name"
-                                type="text"
-                                value={name}
-                                onChangeText={setName}
-                            />
-                        </Input>
-                    </VStack>
-                    <VStack
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                        space="lg"
-                        mt={22}
-                    >
-                        <Input w={270} variant="rounded" >
-                            <InputField
-                                placeholder="Email"
-                                type="text"
-                                value={email}
-                                onChangeText={setEmail}
-                            />
-                        </Input>
-                    </VStack>
-
-                    <VStack
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                        space="lg"
-                        mt={22}
-                    >
-                        <Input
-                            w={270}
-                            variant="rounded"
-                        >
-                            <InputField
-                                placeholder="Password"
-                                type={showPassword ? "text" : "password"}
-                                value={password}
-                                onChangeText={setPassword}
-                            />
-                            <InputSlot
-                                pr="$3"
-                                onPress={handleState}
-                            >
-                                <InputIcon
-                                    as={showPassword ? EyeIcon : EyeOffIcon}
-                                    color="$yellow500"
-                                />
-                            </InputSlot>
-                        </Input>
-                    </VStack>
-                    <VStack
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                        space="lg"
-                        mt={22}
-                    >
-                        <Input
-                            w={270}
-                            variant="rounded"
-                        >
-                            <InputField
-                                placeholder="Password"
-                                type={showPassword ? "text" : "password"}
-                                value={password_confirmation}
-                                onChangeText={setConfirmPassword}
-                            />
-                            <InputSlot
-                                pr="$3"
-                                onPress={handleState}
-                            >
-                                <InputIcon
-                                    as={showPassword ? EyeIcon : EyeOffIcon}
-                                    color="$yellow500"
-                                />
-                            </InputSlot>
-                        </Input>
-                    </VStack>
-
-                    <VStack
-                        display='flex'
-                        alignItems='center'
-                        justifyContent='center'
-                        space="lg"
-                        mt={10}
-                        mb="10%"
-                        >
-                    <Button
-                        bg="$yellow500"
-                        $active-bg="$yellow600"
-                        w={90}
-                        size="sm"
-                        variant="solid"
-                        isDisabled={false}
-                        onPress={onHandleSignUp}
-                    >
-                        <ButtonText>Register</ButtonText>
-                    </Button>
-                    <Link onPress={handleNavigate}>
-                        <LinkText>Already have an account?</LinkText>
-                    </Link>
-                </VStack>
-
-
-            </FormControl>
-        </Box >
-
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting,
+                    }) => (
+                        <FormControl p="$4" borderRadius="$lg">
+                            <VStack alignItems='center' justifyContent='center' space="lg" mt={15}>
+                                <Heading $dark-color="$textLight200" size='2xl' style={{ textAlign: 'center' }}>
+                                    SubstanceTwins
+                                </Heading>
+                                <Text $dark-color="$textLight200" size='xl' style={{ textAlign: 'center' }}>
+                                    Your body must be prepared for any situation, which is why you need supplements.
+                                </Text>
+                                {errors.name && touched.name && <Alert h={10} bgColor="$red300" variant="solid"><AlertText>{errors.name}</AlertText></Alert>}
+                                <Input w={270} variant="rounded">
+                                    <InputField
+                                        placeholder="Name"
+                                        type="text"
+                                        onChangeText={handleChange('name')}
+                                        onBlur={handleBlur('name')}
+                                        value={values.name}
+                                    />
+                                </Input>
+                                {errors.email && touched.email && <Alert h={10} bgColor="$red300" variant="solid"><AlertText>{errors.email}</AlertText></Alert>}
+                                <Input w={270} variant="rounded">
+                                    <InputField
+                                        placeholder="Email"
+                                        type="text"
+                                        onChangeText={handleChange('email')}
+                                        onBlur={handleBlur('email')}
+                                        value={values.email}
+                                    />
+                                </Input>
+                                {errors.password && touched.password && <Alert h={10} bgColor="$red300" variant="solid"><AlertText>{errors.password}</AlertText></Alert>}
+                                <Input w={270} variant="rounded">
+                                    <InputField
+                                        placeholder="Password"
+                                        type={showPassword ? "text" : "password"}
+                                        onChangeText={handleChange('password')}
+                                        onBlur={handleBlur('password')}
+                                        value={values.password}
+                                    />
+                                    <InputSlot pr="$3" onPress={handleState}>
+                                        <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} color="$yellow500" />
+                                    </InputSlot>
+                                </Input>
+                                {errors.password_confirmation && touched.password_confirmation && <Alert h={10} bgColor="$red300" variant="solid"><AlertText>{errors.password_confirmation}</AlertText></Alert>}
+                                <Input w={270} variant="rounded">
+                                    <InputField
+                                        placeholder="Confirm Password"
+                                        type={showPassword ? "text" : "password"}
+                                        onChangeText={handleChange('password_confirmation')}
+                                        onBlur={handleBlur('password_confirmation')}
+                                        value={values.password_confirmation}
+                                    />
+                                    <InputSlot pr="$3" onPress={handleState}>
+                                        <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} color="$yellow500" />
+                                    </InputSlot>
+                                </Input>
+                                <Button
+                                    bg="$yellow500"
+                                    $active-bg="$yellow600"
+                                    w={90}
+                                    size="sm"
+                                    variant="solid"
+                                    onPress={() => handleSubmit()}
+                                    disabled={isSubmitting}
+                                >
+                                    <ButtonText>{isSubmitting ? 'Registering...' : 'Register'}</ButtonText>
+                                </Button>
+                                <Link onPress={handleNavigate}>
+                                    <LinkText>Already have an account? Click here to login</LinkText>
+                                </Link>
+                            </VStack>
+                        </FormControl>
+                    )}
+                </Formik>
+            </Box>
         </>
+    );
+};
 
-    )
-}
 export default Register;
